2b2729517ca39e8a5ea93487898fa0cc20be3606
Sämir Hamadi <samir.hamadi@elisa.fi>
2015-04-21 19:17:51 +0300

    Add wait and notify for operations

--- src/test/java/fi/samssi/Queue.java
+++ src/test/java/fi/samssi/Queue.java
@@ -5,17 +5,34 @@ import java.util.List;
 
 public class Queue {
     private static List<String> messages = new ArrayList<String>();
+    private boolean locked = false;
 
     public synchronized void addMessageToQueue(String message) {
+        waitForIt();
+        locked = true;
         messages.add(message);
+        locked = false;
+        notifyAll();
+    }
+    public synchronized void waitForIt() {
+        while(locked) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public synchronized String popMessageFromQueue() {
+        locked = true;
         if (messages.size() > 0) {
             String message = messages.get(0);
             messages.remove(0);
             return message;
         }
+        locked = false;
+        notifyAll();
         return "";
     }
 }
