6bdfe777d54e476e828c03ff9ff3b16f1af8d5a9
Sämir Hamadi <samir.hamadi@elisa.fi>
2015-04-21 19:52:27 +0300

    Wrap up functionally

--- src/test/java/fi/samssi/Queue.java
+++ src/test/java/fi/samssi/Queue.java
@@ -2,18 +2,34 @@ package fi.samssi;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Queue {
     private static List<String> messages = new ArrayList<String>();
     private boolean locked = false;
 
     public synchronized void addMessageToQueue(String message) {
+        doLockedOperationForBoolean(() -> messages.add(message));
+    }
+
+    private synchronized void doLockedOperationForBoolean(Supplier<Boolean> function) {
         waitForIt();
         locked = true;
-        messages.add(message);
+        function.get();
         locked = false;
         notifyAll();
     }
+
+    private synchronized String doLockedOperationForString(Supplier<String> function) {
+        waitForIt();
+        locked = true;
+        String result = function.get();
+        locked = false;
+        notifyAll();
+        return result;
+    }
+
     public synchronized void waitForIt() {
         while(locked) {
             try {
@@ -24,15 +40,18 @@ public class Queue {
         }
     }
 
+    private synchronized Supplier<String> messagePopper() {
+        return () -> {
+            if (messages.size() > 0) {
+                String message = messages.get(0);
+                messages.remove(0);
+                return message;
+            }
+            return "";
+        };
+    }
+
     public synchronized String popMessageFromQueue() {
-        locked = true;
-        if (messages.size() > 0) {
-            String message = messages.get(0);
-            messages.remove(0);
-            return message;
-        }
-        locked = false;
-        notifyAll();
-        return "";
+        return doLockedOperationForString(messagePopper());
     }
 }
